794de73c842143e14fed1de99b37055b5959b88f
sajjadjawaid <sajjadjawaid@gmail.com>
2024-10-17 01:47:59 -0700

    feat: correct icons in job detail component

--- frontEnd/src/app/Components/main-section/main-section.component.ts
+++ frontEnd/src/app/Components/main-section/main-section.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { JobApplicationService } from '../../services/job_application/job-application.service';
 
 @Component({
   selector: 'app-main-section',
@@ -8,9 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './main-section.component.html',
   styleUrl: './main-section.component.css',
 })
-export class MainSectionComponent {
+export class MainSectionComponent implements OnInit {
   dropdown: boolean = false;
   onClickThreeDots() {
     this.dropdown = !this.dropdown;
+  }
+
+  applications: [] = [];
+  service = inject(JobApplicationService);
+
+  ngOnInit(): void {
+    this.getApplications();
+  }
+
+  getApplications() {
+    this.service.getApplications().subscribe((res: any) => {
+      this.applications = res;
+    });
   }
 }
